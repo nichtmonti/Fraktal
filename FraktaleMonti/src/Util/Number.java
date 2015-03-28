94906bde1b4c7ed6934dc7f402b57bf674e780f0
t628 <t6.28@outlook.com>
2015-03-28 22:23:24 +0100

    fix

--- FraktaleMonti/src/Util/Number.java
+++ FraktaleMonti/src/Util/Number.java
@@ -4,18 +4,18 @@ public class Number implements Expression {
     public complex val;
     
     public Number(complex c){
-    	val=c;
+    	val=new complex(c.getR(), c.getI());
     }
     
 	public complex eval() {
-		return val;
+		return new complex(val.getR(), val.getI());
 	}
 
 	public String toString(){
-		if (val.getR()==0)
-			return val.getI() + "i";
-		else if(val.getI()==0)
+		if (val.getI()==0)
 			return val.getR() + "";
+		else if(val.getR()==0)
+			return val.getI() + "i";
 		return "+ " + val.getR() + " " + val.getI() + "i"; 
 	}
 	
