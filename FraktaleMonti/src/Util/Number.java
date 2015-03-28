35b65525315ccfbfb9bd8be7c95d5bc56a08f771
t628 <t6.28@outlook.com>
2015-03-28 18:50:56 +0100

    fix

--- FraktaleMonti/src/Util/Number.java
+++ FraktaleMonti/src/Util/Number.java
@@ -12,6 +12,10 @@ public class Number implements Expression {
 	}
 
 	public String toString(){
+		if (val.getR()==0)
+			return val.getI() + "i";
+		else if(val.getI()==0)
+			return val.getR() + "";
 		return "+ " + val.getR() + " " + val.getI() + "i"; 
 	}
 	
