66654c87e1b90285be90d6ef5373dc8c45f297f2
ChalkSuey <jonaskreidelmeyer@gmail.com>
2015-03-30 23:11:31 +0200

    Sierpinksi

--- FraktaleMonti/src/Fraktale/Sierpinski.java
+++ FraktaleMonti/src/Fraktale/Sierpinski.java
@@ -11,7 +11,7 @@ public class Sierpinski extends Fraktal{
 	List<vector2d> P = new ArrayList();
 	linearFun lin1 = new linearFun("{{0.5,0},{0,0.5}}","{{5,0}}");
 	linearFun lin2 = new linearFun("{{0.5,0},{0,0.5}}","{{-5,0}}");
-	linearFun lin3 = new linearFun("{{0.5,0},{0,0.5}}","{{0,4.3301}}");
+	linearFun lin3 = new linearFun("{{0.5,0},{0,0.5}}","{{0,5}}");
 	private int iteration = 10;
 	public Sierpinski(double xmin, double xmax, double ymin, double ymax,long scale) {
 		super(xmin, xmax, ymin, ymax, scale);
@@ -42,7 +42,7 @@ public class Sierpinski extends Fraktal{
 		for(int i=0;i<pixels.length;i++)for(int j=0;j<pixels[i].length;j++)pixels[i][j]=0x000000;
 		for(vector2d x : P){
 			try{
-				for(int i = 0;i<9;i++)pixels[(int)((x.getR()-xmin)*scale)][(int)((x.getI()-ymin)*scale)]=0xffff55;
+				for(int i = 0;i<9;i++)pixels[(int)((x.getR()-xmin)*scale)][(int)((x.getI()-ymin)*scale)]=0xffff00;
 			}catch(Exception e){};
 		}
 	}
